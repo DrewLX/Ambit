60228a4edd84ca56fd58f8b72272941304cbc8b8
Drew Perry <drew@eventengineering.co.uk>
2016-11-13 20:00:42 +0000

    Added electron-log module

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const electron = require('electron')
 const {app, BrowserWindow} = electron
 
+var log = require('electron-log');
+
+
+log.info('Period App Launched');
 
 app.on('ready', () => {
 	let win = new BrowserWindow({width:1000, height:600})
