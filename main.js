0e58095e6afb694de5395ed9010c20ea6073c68c
Drew Perry <drew@eventengineering.co.uk>
2016-11-13 22:58:08 +0000

    Added OSC.js module

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ var log = require('electron-log');
 log.info('Period App Launched');
 
 app.on('ready', () => {
-	let win = new BrowserWindow({width:1000, height:600})
+	let win = new BrowserWindow({width:800, height:600})
 	win.loadURL('file://' + __dirname + '/index.html')
 
-	win.webContents.openDevTools()
+	//win.webContents.openDevTools()
 })
