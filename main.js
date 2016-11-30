2e7782fb5f6530276e34027f8319820ee0924891
Drew Perry <drew@eventengineering.co.uk>
2016-11-30 22:15:36 +0000

    Some basic styling

--- main.js
+++ main.js
@@ -10,5 +10,5 @@ app.on('ready', () => {
 	let win = new BrowserWindow({width:800, height:600})
 	win.loadURL('file://' + __dirname + '/control.html')
 
-	//win.webContents.openDevTools()
+	win.webContents.openDevTools()
 })
